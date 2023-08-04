512ad4c7baaf41367799eb4ce2e18b027d2409fe
Juliano Santos <julianosantosdev@outlook.com>
2023-08-04 18:26:06 -0300

    style: add contact list hover animation

--- kontakts/src/components/Contacts/ContactsCards/styles.ts
+++ kontakts/src/components/Contacts/ContactsCards/styles.ts
@@ -1,20 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ContactCardLi = styled.li`
-height: 3rem;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 0 1rem;
-border-radius: inherit;
-width: 100%;
-background-color: var(--color-grey-4);
-transition: 200ms;
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  border-radius: inherit;
+  width: 100%;
+  background-color: var(--color-grey-4);
+  transition: 200ms;
+  cursor: pointer;
 
-&:hover {
+  &:hover {
     background-color: var(--color-secondary);
-}
+    scale: 103%;
+  }
+`;
 
-`
-
-export { ContactCardLi }
+export { ContactCardLi };
